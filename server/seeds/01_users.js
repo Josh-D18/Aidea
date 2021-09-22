e49c7a7b84bf742245dafc4b1d0bf36dc6fe17ba
unknown <jjdate@live.com>
2021-09-22 17:23:46 -0400

    dev change

--- server/seeds/01_users.js
+++ server/seeds/01_users.js
@@ -1,18 +1,26 @@
+const bcrypt = require("bcryptjs");
+
 const userData = [
   {
     id: 1,
     user_name: "Katsu",
-    password: "hello",
+    password: bcrypt.hash("hello", 8).then((hashPassword) => {
+      return hashPassword.toString();
+    }),
   },
   {
     id: 2,
     user_name: "Nathan",
-    password: "hello",
+    password: bcrypt.hash("hello", 8).then((hashPassword) => {
+      return hashPassword.toString();
+    }),
   },
   {
     id: 3,
     user_name: "Stacy",
-    password: "hello",
+    password: bcrypt.hash("hello", 8).then((hashPassword) => {
+      return hashPassword.toString();
+    }),
   },
 ];
 
